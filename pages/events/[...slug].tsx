eeb5480ebb6490394bea551cd7491dc96f49a016
Nik68rus <nik200x@mail.ru>
2022-11-26 17:00:00 +0300

    switched partly to clien side data fetching

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next';
-// import { useRouter } from 'next/router';
-// import { useState, useEffect } from 'react';
+import useSwr from 'swr';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import EventList from '../../components/events/EventList';
 import ResultsTitle from '../../components/events/ResultsTitle';
 import Button from '../../components/ui/Button';
@@ -9,56 +10,39 @@ import { getFilteredEvents } from '../../helpers/api-util';
 import { IEvent } from '../../types';
 import { ParsedUrlQuery } from 'querystring';
 
-type Props = {
-  events: IEvent[];
-  badFilter: boolean;
-  date: {
-    year: number;
-    month: number;
-  };
-};
+type Props = {};
 
 interface Params extends ParsedUrlQuery {
   slug: string[];
 }
 
-function FilteredEventsPage({ events, badFilter, date }: Props) {
-  if (!events) {
+function FilteredEventsPage({}: Props) {
+  const router = useRouter();
+
+  const slug = router.query.slug as string[];
+
+  const [fetchedEvents, setFetchedEvents] = useState<IEvent[]>([]);
+
+  const { data, error } = useSwr(
+    'https://nextjs-course-ef524-default-rtdb.firebaseio.com/events.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const events: IEvent[] = [];
+      for (const id in data) {
+        events.push({ id, ...data[id] });
+      }
+
+      setFetchedEvents(events);
+    }
+  }, [data]);
+
+  if (!fetchedEvents || !slug) {
     return <p className="center">Loading...</p>;
   }
 
-  if (badFilter) {
-    return (
-      <div className="center">
-        <ErrorAlert>Invalid filter!</ErrorAlert>
-        <Button link="/events">Show All events</Button>
-      </div>
-    );
-  }
-
-  const dateVal = new Date(date.year, date.month - 1);
-
-  if (events && !events.length) {
-    return (
-      <>
-        <ResultsTitle date={dateVal} />
-        <ErrorAlert>No events this month!</ErrorAlert>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <ResultsTitle date={dateVal} />
-      <EventList items={events} />
-    </>
-  );
-}
-
-export const getServerSideProps: GetServerSideProps<Props, Params> = async (
-  context
-) => {
-  const { slug } = context.params!;
   const [year, month] = slug;
   let yearNum = +year;
   let monthNum = +month;
@@ -70,23 +54,76 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async (
     yearNum > 2030 ||
     yearNum < 2020 ||
     monthNum < 1 ||
-    monthNum > 12
+    monthNum > 12 ||
+    error
   ) {
-    badFilter = true;
-    yearNum = 0;
-    monthNum = 0;
+    return (
+      <div className="center">
+        <ErrorAlert>Invalid filter!</ErrorAlert>
+        <Button link="/events">Show All events</Button>
+      </div>
+    );
   }
 
-  const events: IEvent[] = badFilter
-    ? []
-    : await getFilteredEvents({ year: yearNum, month: monthNum });
-  return {
-    props: {
-      events,
-      badFilter,
-      date: { year: yearNum, month: monthNum },
-    },
-  };
-};
+  const dateVal = new Date(yearNum, monthNum - 1);
+
+  let filteredEvents = fetchedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === yearNum &&
+      eventDate.getMonth() === monthNum - 1
+    );
+  });
+
+  if (fetchedEvents.length && !filteredEvents.length) {
+    return (
+      <>
+        <ResultsTitle date={dateVal} />
+        <ErrorAlert>No events this month!</ErrorAlert>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ResultsTitle date={dateVal} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+}
+
+// export const getServerSideProps: GetServerSideProps<Props, Params> = async (
+//   context
+// ) => {
+//   const { slug } = context.params!;
+//   const [year, month] = slug;
+//   let yearNum = +year;
+//   let monthNum = +month;
+//   let badFilter = false;
+
+//   if (
+//     isNaN(yearNum) ||
+//     isNaN(monthNum) ||
+//     yearNum > 2030 ||
+//     yearNum < 2020 ||
+//     monthNum < 1 ||
+//     monthNum > 12
+//   ) {
+//     badFilter = true;
+//     yearNum = 0;
+//     monthNum = 0;
+//   }
+
+//   const events: IEvent[] = badFilter
+//     ? []
+//     : await getFilteredEvents({ year: yearNum, month: monthNum });
+//   return {
+//     props: {
+//       events,
+//       badFilter,
+//       date: { year: yearNum, month: monthNum },
+//     },
+//   };
+// };
 
 export default FilteredEventsPage;
